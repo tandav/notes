74fe7766bb1d56a970b26955cfa431f14f11c617
Alexander Rodionov <tandav@tandav.me>
2022-05-05 17:30:45 +0300

    test create more users

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,8 @@
 import pytest
 from http import HTTPStatus
 from notes import models
+from faker import Faker
+fake = Faker()
 
 
 @pytest.mark.parametrize('table', [
@@ -13,11 +15,8 @@ def test_tables_empty(db, table):
     assert db.query(table).count() == 0
 
 
-def test_create_item(client):
-    response = client.post(
-        "/users/",
-        json={"username": "test_user", "password": "test_password"},
-    )
+def test_create_user(client):
+    response = client.post("/users/", json={"username": "test_user", "password": "test_password"})
     assert response.ok, response.json()
     j = response.json()
     assert j.pop('created_time')
@@ -27,6 +26,12 @@ def test_create_item(client):
         'notes': [],
         'username': 'test_user',
     }
+
+    # create more users
+    for _ in range(2):
+        username = fake.user_name()
+        password = fake.password(length=32, special_chars=False)
+        assert client.post("/users/", json={"username": username, "password": password}).ok
 
 
 def test_username_already_registred(client):
