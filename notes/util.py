a1ceafb8b7282a124a55b692868bd59512f65c9f
Alexander Rodionov <tandav@tandav.me>
2022-05-12 18:40:55 +0300

    use colorutil

--- notes/util.py
+++ notes/util.py
@@ -1,6 +1,6 @@
 import datetime
 import string
-import colorsys
+import colortool
 
 
 def _ago(e) -> str:
@@ -66,69 +66,10 @@ def header(
     return '\n'.join(items)
 
 
-def minmax_scaler(value, oldmin, oldmax, newmin=0.0, newmax=1.0) -> float:
-    '''
-    >>> minmax_scaler(50, 0, 100, 0.0, 1.0)
-    0.5
-    >>> minmax_scaler(255, 0, 255, 0.0, 1.0)
-    1.0
-    '''
-    return (value - oldmin) * (newmax - newmin) / (oldmax - oldmin) + newmin
-
-
-RGBInt = tuple[int, int, int]
-RGBFloat = tuple[float, float, float]
-
-
-def to_rgb_int(color: str) -> RGBInt:
-    return (
-        int(color[1:3], base=16),
-        int(color[3:5], base=16),
-        int(color[5:7], base=16),
-    )
-
-
-def to_rgb_float(color: RGBInt) -> RGBFloat:
-    return (
-        minmax_scaler(color[0], 0, 255),
-        minmax_scaler(color[1], 0, 255),
-        minmax_scaler(color[2], 0, 255),
-    )
-
-
-def hls_to_css_hex(h, l, s) -> str:
-    r, g, b = colorsys.hls_to_rgb(h, l, s)
-    r = int(minmax_scaler(r, 0, 1, 0, 255))
-    g = int(minmax_scaler(g, 0, 1, 0, 255))
-    b = int(minmax_scaler(b, 0, 1, 0, 255))
-    return f'#{r:02X}{g:02X}{b:02X}'
-
-
-def font_border_colors(
-    color: str,
-    font_threshold: float = 0.5,
-    border_threshold: float = 0.9,
-) -> tuple[str, str]:
-    """
-    determine the font color to be either black or white depending on the background color
-    https://css-tricks.com/switch-font-color-for-different-backgrounds-with-css/
-
-    :param color: hex string in #4bb9ac format
-    :param font_threshold: 0..1 float. lightness value below the threshold will result in white, any above will result in black
-    :param border_threshold: 0..1 float. lightness value below the threshold will result the border-color as same, any above 30% darker shade of the same color
-    :return: font_color, border_color in css hex string format #4bb9ac
-    """
-    rgb = to_rgb_float(to_rgb_int(color))
-    h, l, s = colorsys.rgb_to_hls(*rgb)
-    font_color = '#ffffff' if l < font_threshold else '#000000'
-    border_color = color if l < border_threshold else hls_to_css_hex(h, l * 0.7, s)
-    return font_color, border_color
-
-
 def tags_css(tags) -> str:
     items = []
     for tag in tags:
-        font_color, border_color = font_border_colors(tag.color)
+        font_color, border_color = colortool.font_border_colors(tag.color)
         items.append(f'''
         [id="{tag.name}"] {{
             background-color: {tag.color};
