2ba47b890e5cecef6836dfeddcccfd083eb4e373
Alexander Rodionov <tandav@tandav.me>
2022-07-16 16:01:45 +0300

    test_public_auth_user_notes_are_accessible_for_anon

--- tests_v2/test_auth.py
+++ tests_v2/test_auth.py
@@ -33,7 +33,12 @@ def test_anon_notes_are_accessible_for_auth_user(client, create_users):
     assert [n['id'] for n in client.get('/users/anon/notes', auth=auth0).json()] == n_ids
 
 
-def test_public_auth_user_notes_are_accessible_for_anon(client): pass
+def test_public_auth_user_notes_are_accessible_for_anon(client, create_users):
+    auth0, auth1 = create_users
+    n_ids = [client.post('/notes/', json={'is_private': False}, auth=auth0).json()['id'] for _ in range(3)]
+    assert [n['id'] for n in client.get(f'/users/{auth0[0]}/notes').json()] == n_ids
+
+
 def test_public_auth_user_notes_are_accessible_for_other_auth_user(client): pass
 def test_private_auth_user_notes_are_not_accessible_for_anon(client): pass
 def test_private_auth_user_notes_are_not_accessible_for_other_auth_user(client): pass
