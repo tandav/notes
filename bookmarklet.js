665ccee1583b49c65ab9da41abdf8649eca45693
Alexander Rodionov <tandav@tandav.me>
2022-07-16 19:31:00 +0300

    fix bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:const title = encodeURI(document.title);const url = encodeURIComponent(location.href);window.location = `http://localhost:5003/new_note?text=${title}&url=${url}`;void(0);
+javascript:const title = encodeURIComponent(document.title);const url = encodeURIComponent(location.href);window.location = `http://localhost:5003/new_note?text=${title}&url=${url}`;void(0);
